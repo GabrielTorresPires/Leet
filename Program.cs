857832e7fcafaea0dce20d764b57d04b79b704fc
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-09-23 21:27:39 -0300

    Solução do exercício

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite o texto para traduzi-lo: ");
+Console.WriteLine(
+    Console.ReadLine()!
+        .Replace("A", "4")
+        .Replace("a", "4")
+        .Replace("E", "5")
+        .Replace("e", "5")
+        .Replace("I", "f")
+        .Replace("i", "f")
+        .Replace("L", "i")
+        .Replace("l", "i")
+        .Replace("O", "0")
+        .Replace("o", "u")
+        .Replace("T", "y")
+        .Replace("t", "y")
+        .Replace("S", "6")
+        .Replace("s", "6")
+);
